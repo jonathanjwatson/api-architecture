443addb3cc5e4423ef72668bd0dc668717da9a04
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-10-26 09:42:21 -0400

    login complete

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,9 +7,9 @@ const db = require("../models");
 // Sign UP
 
 router.post("/api/signup", (req, res) => {
-    const {emailAddress, password} = req.body
-    console.log(emailAddress);
-    console.log(password);
+  const { emailAddress, password } = req.body;
+  console.log(emailAddress);
+  console.log(password);
   if (!emailAddress.trim() || !password.trim()) {
     res.status(400);
   } else {
@@ -40,5 +40,53 @@ router.post("/api/signup", (req, res) => {
 });
 
 // Login
+
+router.post("/api/login", (req, res) => {
+  const { emailAddress, password } = req.body;
+  db.User.findOne({ emailAddress: emailAddress })
+    .then((foundUser) => {
+      if (foundUser) {
+        console.log(foundUser);
+        console.log("Hashed password from DB", foundUser.password);
+        console.log("Plain text password from user", password);
+        bcrypt
+          .compare(password, foundUser.password)
+          .then(function (result) {
+            // result == true
+            console.log("The passwords match: ", result);
+            if (result) {
+              // TODO: send a jwt back as data instead.
+              res.json({
+                error: false,
+                data: null,
+                message: "Successfully logged in.",
+              });
+            } else {
+              res.status(401).json({
+                error: true,
+                data: null,
+                message: "Failed to sign in.",
+              });
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(401).json({
+              error: true,
+              data: null,
+              message: "Failed to sign in.",
+            });
+          });
+      }
+    })
+    .catch((err) => {
+        console.log(err);
+      res.status(500).json({
+        error: true,
+        data: null,
+        message: "Failed to sign in.",
+      });
+    });
+});
 
 module.exports = router;
