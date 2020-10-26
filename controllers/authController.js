5e4ff955df4279843956dca17c293d53cb1b9867
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-10-26 10:00:25 -0400

    Jwt returned on login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const db = require("../models");
 
@@ -42,26 +43,40 @@ router.post("/api/signup", (req, res) => {
 // Login
 
 router.post("/api/login", (req, res) => {
+  // Pull user provided email address and password from the body.
   const { emailAddress, password } = req.body;
+  // See if there is a matching user in the database.
   db.User.findOne({ emailAddress: emailAddress })
     .then((foundUser) => {
       if (foundUser) {
         console.log(foundUser);
         console.log("Hashed password from DB", foundUser.password);
         console.log("Plain text password from user", password);
+        // If there is a matching user, compare the plaintext password with the stored, hashed password.
         bcrypt
           .compare(password, foundUser.password)
           .then(function (result) {
             // result == true
             console.log("The passwords match: ", result);
             if (result) {
-              // TODO: send a jwt back as data instead.
+              // If the passwords match, send back success.
+              // TODO: send a jwt back as data instead. DONE
+              // TODO: lock down the token with a time frame
+              const token = jwt.sign(
+                {
+                  emailAddress: foundUser.emailAddress,
+                  firstName: foundUser.firstName,
+                  lastName: foundUser.lastName,
+                },
+                "shhhhh"
+              );
               res.json({
                 error: false,
-                data: null,
+                data: token,
                 message: "Successfully logged in.",
               });
             } else {
+              // If the passwords don't match, send back an error.
               res.status(401).json({
                 error: true,
                 data: null,
@@ -80,7 +95,7 @@ router.post("/api/login", (req, res) => {
       }
     })
     .catch((err) => {
-        console.log(err);
+      console.log(err);
       res.status(500).json({
         error: true,
         data: null,
