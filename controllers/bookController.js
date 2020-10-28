d1e2a55e6b98772244b31377dd700434f59d6df6
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-10-28 09:52:44 -0400

    POST/PUT routes for books complete

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -51,17 +51,96 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  db.Book.create(req.body).then((newBook) => {
-    res.json(newBook);
+  console.log(req.headers);
+  if (!req.headers.authorization) {
+    return res.status(401).json({
+      error: true,
+      data: null,
+      message: "Unauthorized.",
+    });
+  }
+  jwt.verify(req.headers.authorization, process.env.SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err);
+      return res.status(401).json({
+        error: true,
+        data: null,
+        message: "Invalid token.",
+      });
+    } else {
+      console.log(decoded);
+      const newBook = {
+        title: req.body.title,
+        pages: req.body.pages,
+        author: req.body.author,
+        creatorId: decoded._id,
+      };
+      db.Book.create(newBook).then((newBook) => {
+        res.json(newBook);
+      });
+    }
   });
 });
 
 router.put("/:id", (req, res) => {
-  db.Book.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(
-    (updatedBook) => {
-      res.json(updatedBook);
+    // To update a book, you must pass in an authorization header. 
+    // If no authorization header is provided, return 401. 
+  if (!req.headers.authorization) {
+    return res.status(401).json({
+      error: true,
+      data: null,
+      message: "Unauthorized.",
+    });
+  }
+  // If jwt is provided as an authorization header, make sure it is valid. 
+  jwt.verify(req.headers.authorization, process.env.SECRET, (err, decoded) => {
+      // If jwt is invalid (for any reason) return 401.
+    if (err) {
+      console.log(err);
+      return res.status(401).json({
+        error: true,
+        data: null,
+        message: "Invalid token.",
+      });
+    } else {
+        // If jwt is valid, pull the allowable fields from the body and update the book. 
+      console.log(decoded);
+      const updatedBook = {
+        title: req.body.title,
+        pages: req.body.pages,
+        author: req.body.author,
+      };
+      // Restrict updates where the creatorId is equal to the user-provided token _id.
+      db.Book.findOneAndUpdate(
+        { _id: req.params.id, creatorId: decoded._id },
+        updatedBook,
+        { new: true }
+      )
+        .then((updatedBook) => {
+            if(!updatedBook){
+                res.status(404).json({
+                    error: true,
+                    data: null,
+                    message: "Unable to find that book."
+                })
+            }else{
+                res.json({
+                    error: false,
+                    data: updatedBook,
+                    message: "Successfully updated book."
+                });
+            }
+          
+        })
+        .catch((err) => {
+          res.status(500).json({
+            error: true,
+            data: null,
+            message: "An error occurred updating your book.",
+          });
+        });
     }
-  );
+  });
 });
 
 router.delete("/:id", (req, res) => {
